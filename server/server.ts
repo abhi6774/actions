8ab749bb1b0202f078c79a88ec9aa4efed4f7b1a
glxymesh <abhishekmourya6774@gmail.com>
2023-03-27 01:46:23 +0530

    Adding release workflow

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import path from "path";
-import { authRouter } from "routers";
+import { authRouter, profileRouter } from "./routers";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/home", (req, res) => {
   res.send("Home Route");
 })
 
 app.use("/auth", authRouter);
-app.use("/profile", authRouter);
+app.use("/profile", profileRouter);
 
 app.listen(2000, () => {
   console.log("App is Listening on http://localhost:2000");
