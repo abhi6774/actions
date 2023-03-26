478ea37dd8905bb234da45251ecc32e19278afbe
glxymesh <abhishekmourya6774@gmail.com>
2023-03-27 00:05:31 +0530

    style update

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(2000, () => {
   console.log("App is Listening on http://localhost:2000");
